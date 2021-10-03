e4444d415a20a6eae0e92ecaea1bced8631b36d5
Louis <>
2021-10-03 14:52:06 +0200

    volume update

--- src/pages/volume.js
+++ src/pages/volume.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useHistory } from 'react-router';
 
+import "./volume.css"
+
 function Volume() {
 
     const history = useHistory();
@@ -13,10 +15,10 @@ function Volume() {
             <h1>Volume per destination</h1>
             <p>here will be displayed date and vehicle immatriculation</p>
             <p>here the destination name</p>
-            <form>
-                <p>Lenght</p>
-                <p>Width</p>
-                <p>Height</p>
+            <form className = "volumeForm">
+                <p><label className = "fromLabel">Lenght: </label><input type = "number" /></p>
+                <p><label className = "fromLabel">Width: </label><input type = "number" /></p>
+                <p><label className = "fromLabel">Height: </label><input type = "number" /></p>
             </form>
             <button>Add another volume</button>
             <p>Total Volume {total} m3</p>
